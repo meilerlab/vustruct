c8c3de8ffb9d87916521302a895e5d1ba3cb27b2
Chris Moth <Chris.Moth@vanderbilt.edu>
2018-11-20 15:17:54 -0600

    Select production, or development mode, depending on location of psb_rep.bash file when sourced

--- psb_prep.bash
+++ psb_prep.bash
@@ -1,21 +1,42 @@
 echo "Preparing PDB Pipeline PATH and environment settings"
 echo "This file should only be sourced from a bash or sh shell"
-echo 
+echo
+if [[ $0 != $BASH_SOURCE ]]; then
+PIPELINE_ROOT=$(readlink -f `dirname $BASH_SOURCE`)
+echo "Script in $PIPELINE_ROOT was correctly sourced"
+if [[ $PIPELINE_ROOT = '$PIPELINE_ROOT' ]]; then
+isProduction=true
+else
+isProduction=false
+false
+fi
+if $isProduction ; then
+echo "PRODUCTION INITIALIZATION"
+else
+echo "** DEVELOPMENT (NOT PRODUCTION) ENVIRONMENT INITIALIZING **"
+fi
+else
+echo "Script cannot be run as you invoked it.  You must instead 'source $(readlink -f $0)'"
+exit
+fi
+
 # Directories that must be added to your path
-newPATH=/dors/capra_lab/users/psbadmin/anaconda2/bin
-newPATH=$newPATH:/dors/capra_lab/users/psbadmin/bin
-newPATH=$newPATH:/dors/capra_lab/users/psbadmin/pdbmap
-newPATH=$newPATH:/dors/capra_lab/users/psbadmin/pathprox
+newPATH=$PIPELINE_ROOT/anaconda2/bin
+newPATH=$newPATH:$PIPELINE_ROOT/bin
+newPATH=$newPATH:$PIPELINE_ROOT/pdbmap
+newPATH=$newPATH:$PIPELINE_ROOT/pathprox
 newPATH=$newPATH:/dors/capra_lab/opt/ensembl-tools-release-87/scripts/variant_effect_predictor
 newPATH=$newPATH:/dors/capra_lab/opt/ensembl-tools/release-87/scripts/id_history_converter
 newPATH=$newPATH:/dors/capra_lab/bin/vcftools/bin
 newPATH=$newPATH:/dors/capra_lab/bin/vcftools/perl
+newPATH=$newPATH:/dors/capra_lab/bin/wkhtmltox/bin
 echo -ne "Prepending to PATH:\n\t"
 echo $newPATH | sed 's/:/\n\t/g'
 PATH=$newPATH:$PATH
 export PATH
 # echo PATH=$PATH
 
+psbbin=$PIPELINE_ROOT/bin
 # Skip a line
 echo
 # Make _SURE_ that user has Perl 5 in their environment
@@ -38,16 +59,22 @@ echo $newPERL5LIB | sed 's/:/\n\t/g'
 export PERL5LIB=$newPERL5LIB:${PERL5LIB}
 
 echo
-# newPYTHONPATH=/dors/capra_lab/users/psbadmin/pdbmap:/dors/capra_lab/psbadmin/bin
-newPYTHONPATH=/dors/capra_lab/users/psbadmin/pdbmap:/dors/capra_lab/users/psbadmin/bin
+# newPYTHONPATH=$PIPELINE_ROOT/pdbmap:/dors/capra_lab/psbadmin/bin
+newPYTHONPATH=$PIPELINE_ROOT/pdbmap:$PIPELINE_ROOT/bin
 echo -ne "Prepending to PYTHONPATH:\n\t"
 echo $newPYTHONPATH | sed 's/:/\n\t/g'
 export PYTHONPATH=$newPYTHONPATH:$PYTHONPATH
 
 echo
-export UDN=/dors/capra_lab/projects/psb_collab/UDN
-echo "\$UDN set to $UDN"
 
+if $isProduction; then
+export UDN=/dors/capra_lab/projects/psb_collab/UDN
 export PS1='\r\n\[$(/usr/bin/tput setaf 15; /usr/bin/tput setab 4)\]   PSB Pipeline    host:\h  user:\u         \[$(/usr/bin/tput sgr0)\]\r\n\w\$ '
+else
+export UDN=/dors/capra_lab/users/mothcw/UDNtests
+# export PS1='\[$(/usr/bin/tput setaf 15; /usr/bin/tput setab 1)\] DEV \[$(/usr/bin/tput sgr0)\] '$PS1
+export PS1='DEV '$PS1
+fi
+echo "\$UDN set to $UDN"
 
 fi
