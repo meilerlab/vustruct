298b9b678402c28d2b98c439195883ca22a6ba1d
ChrisMoth <chris.moth@vanderbilt.edu>
2020-08-24 19:59:33 -0500

    Improve status updating of calculations in the ddg repository. Use the new shared ddg_load_structure_module for intial loads and structure file analysis #16

--- bin/ddg_run.py
+++ bin/ddg_run.py
@@ -45,6 +45,7 @@ from Bio import BiopythonWarning
 from Bio.PDB.PDBExceptions import PDBConstructionWarning
 
 from psb_shared import psb_config
+from psb_shared.ddg_load_structure import ddg_load_structure,LoadStructureError
 from psb_shared.ddg_repo import DDG_repo
 from psb_shared import ddg_clean
 from psb_shared.ddg_monomer import DDG_monomer
@@ -60,12 +61,7 @@ RESOLUTION_QUALITY_MAX=2.5  # Only structures with resolution < 2.5 are routed t
 ## Function Definitions ##
 capra_group = grp.getgrnam('capra_lab').gr_gid
 
-def load_structure(id,coord_filename):
-    with gzip.open(coord_filename,'rt') if coord_filename.split('.')[-1]=="gz" else open(coord_filename,'r') as fin:
-        warnings.filterwarnings('ignore',category=PDBConstructionWarning)
-        structure = PDBParser().get_structure(id,fin)
-        warnings.resetwarnings()
-    return structure
+
 
 ch = logging.StreamHandler()
 LOGGER = logging.getLogger()
@@ -80,6 +76,7 @@ LOGGER.setLevel(logging.DEBUG)
 ch.setLevel(logging.INFO)
 
 cmdline_parser = psb_config.create_default_argument_parser(__doc__, os.path.dirname(os.path.dirname(__file__)),
+                                                           ".",
                                                            add_help=True)
 
 cmdline_parser.add_argument(
@@ -125,40 +122,6 @@ cmdline_parser.add_argument("--label", type=str, default='',
 #                             help="Overwrite previous results. Otherwise, exit with error")
 
 LOGGER.info("Command: %s" % ' '.join(sys.argv))
-
-
-# def __info_update(info):
-#     new_info_filename = os.path.join(statusdir, "info.new")
-#     with open(new_info_filename, 'w') as f:
-#         f.write(info + '\n')
-#     final_info_filename = os.path.join(statusdir, "info")
-#     os.rename(new_info_filename, final_info_filename)
-#     LOGGER.info("%s now contains: %s" % (final_info_filename, info))
-# 
-# 
-# def sys_exit_failure(info):
-#     __info_update(info)
-#     new_progress_filename = os.path.join(statusdir, "progress.new")
-#     with open(new_progress_filename, 'w') as f:
-#         f.write("%s: %s\n" % (__file__, inspect.currentframe().f_back.f_lineno))
-#     os.rename(new_progress_filename, '%s/progress' % statusdir)
-#     # Mark this job as failed
-#     fail_filename = os.path.join(statusdir, "FAILED")
-#     open(fail_filename, 'w').close()
-#     LOGGER.critical("Creating FAILURE file %s" % fail_filename)
-#     sys.exit(info)
-# 
-# 
-# def statusdir_info(info):
-#     __info_update(info)
-#     new_progress_filename = os.path.join(statusdir, "progress.new")
-#     with open(new_progress_filename, 'w') as f:
-#         f.write("%s: %s\n" % (__file__, inspect.currentframe().f_back.f_lineno))
-#     os.rename(new_progress_filename, '%s/progress' % statusdir)
-#     return info
-
-
-
 
 args = cmdline_parser.parse_args()
 
@@ -238,46 +201,6 @@ def test_completed_earlier():
 
 test_completed_earlier()
 
-def mine_structure_info_from_mmCIF(mmCIF_dict):
-    method = mmCIF_dict['_exptl.method'][0]
-    pdb_id = method,mmcif_dict['_entry.id']
-    deposition_date = None
-
-    raw_deposition_date = mmCIF_dict.get('_pdbx_database_status.recvd_initial_deposition_date',None)
-    if raw_deposition_date: # Split into YYYY-MM-DD
-        raw_YYYY_MM_DD = raw_deposition_date[0].split('-')
-        if len(raw_YYYY_MM_DD) == 3:
-            deposition_date = datetime.datetime(
-                int(raw_YYYY_MM_DD[0]),int(raw_YYYY_MM_DD[1]),int(raw_YYYY_MM_DD[2]))
-
-        if not deposition_date:
-            LOGGER.warning("Deposition date %s apparently not YYYY-MM-DD",raw_deposition_date)
-
-    resolution = None # Not applicable is correct for NMR structures
-    if method == 'X-RAY DIFFRACTION':
-        resolution_keys = ['_refine.ls_d_res_high','_reflns.d_resolution_high','_refine_hist.d_res_high']
-        ethod = 'X-RAY'
-    elif method == 'ELECTRON MICROSCOPY':
-        method = 'EM'
-        resolution_keys = ['_em_3d_reconstruction.resolution']
-    elif method.find('NMR') > -1: # resolution is not applicable for nmr
-        resolution_keys = []
-    else:
-        resolution_keys = ['_refine.ls_d_res_high','_reflns.d_resolution_high','_refine_hist.d_res_high']
-        LOGGER.critical('%s Experimental method for %s is unknown'%(method,pdb_id))
-
-    for resolution_key in resolution_keys:
-        if resolution_key in mmCIF_dict:
-            # Congratulations.  You found the old REMARK 2 RESOLUTION entry.... or so we hope.
-            resolution = float(mmCIF_dict[resolution_key][0])
-            break
-    if not resolution and method.find('X-RAY') > 0:
-        LOGGER.warning("pdb %s is method=%s with no resolution entry"%(pdb_id,method))
-
-    structure_info_dict =  {'method': method, 'deposition_date': deposition_date}
-    if resolution:
-        structure_info_dict['resolution'] = resolution
-    return structure_info_dict
 
 
 ddg_repo.psb_status_manager.clear_status_dir()
@@ -290,95 +213,17 @@ residue_to_clean_xref = ddg_repo.residue_to_clean_xref
 structure_info_dict = {}
 if residue_to_clean_xref:
     structure_info_dict = ddg_repo.structure_config
-if not structure_info_dict:
-    # We need to load the structure, clean it, and save the xref
-    if args.pdb:
-        # Load mmCIF from pdb repository
-        structure_id = args.pdb.lower()
-        original_structure_filename = os.path.join(
-            config_dict['pdb_dir'],
-            'structures',
-            'divided',
-            'mmCIF',
-            structure_id[1:3],
-            "%s.cif.gz" % structure_id)
-        original_structure_type = 'mmCIF'
-    elif args.swiss:
-        PDBMapSwiss.load_swiss_INDEX_JSON(config_dict['swiss_dir'],config_dict['swiss_summary']);
-        swiss_info = PDBMapSwiss.get_info(args.swiss)
-        assert swiss_info,"%s was not found in the swiss INDEX JSON file"%args.swiss
-        original_structure_type = 'pdb'
-        original_structure_filename = PDBMapSwiss.get_coord_file(args.swiss)
-        structure_id = swiss_info['modelid']
-        structure_info_dict['pdb_template'] = swiss_info['template']
-        # The swiss template info has source PDB chain in the last position
-        pdb_template_chain = structure_info_dict['pdb_template'][-1]
-        assert pdb_template_chain == args.chain,\
-            "With swiss models, chain on command line %s must match modelled chain %s"%(
-                args.chain,pdb_template_chain)
-        remark3_metrics = PDBMapSwiss.load_REMARK3_metrics(structure_id)
-
-        if not DDG_monomer.evaluate_swiss(structure_id,remark3_metrics):
-            ddg_repo.psb_status_manager.sys_exit_failure("Terminating as %s of insufficient quality for ddg monomer"%args.swiss)
-
-        structure_info_dict['method'] = remark3_metrics['mthd']
-        structure_info_dict['template_identity'] = float(remark3_metrics['sid'])
-    elif args.modbase:  # This is a ENSP.... modbase file.  Could be either Modbase13 or 16 - so look in both places
-        original_structure_type = 'pdb'
-        modbase16 = PDBMapModbase2016(config_dict)
-        original_structure_filename = modbase16.get_coord_file(args.modbase)
-        if os.path.exists(original_structure_filename):
-            structure = load_structure(args.modbase, original_structure_filename)
-        else:
-            modbase13 = PDBMapModbase2013(config_dict)
-            original_structure_filename = modbase13.get_coord_file(args.modbase)
-            if os.path.exists(original_structure_filename):
-                structure = load_structure(args.modbase, original_structure_filename)
-            else:
-                ddg_repo.psb_status_manager.sys_exit_Failure(
-                   "Modbase model id %s not found in modbase2013/2016 directories"%\
-                        args.modbase)
-
-        if not DDG_monomer.evaluate_modbase(original_structure_filename):
-            ddg_repo.psb_status_manager.sys_exit_failure("Terminating as %s of insufficient quality for ddg monomer"%args.modbase)
-
-
-    if not os.path.exists(original_structure_filename):
-        exit_str = "%s: No structure file %s"%(ddg_repo.structure_id, original_structure_filename)
-        ddg_repo.psb_status_manager.sys_exit_failure(exit_str)
-
-    # Open a file handle to our structure
-    # LOGGER.debug("Loading orignal structure %s", original_structure_filename)
-    LOGGER.info("Loading structure id=%s from %s"%(structure_id,original_structure_filename))
-    structure_fin = None
-    if original_structure_filename.endswith(".gz"):
-        structure_fin = gzip.open(original_structure_filename, 'rt')
-    else:
-        structure_fin = open(original_structure_filename, 'rt')
-
-    mmcif_dict = None
-    if original_structure_type == 'mmCIF':
-        mmCIF_parser = MMCIFParser() # QUIET=True)
-        structure = mmCIF_parser.get_structure(structure_id, structure_fin)
-        structure_fin.close()
-
-        # DDG_Cleaner uses the mmcif information to remove non-species tags in PDB files
-        mmcif_dict = mmCIF_parser._mmcif_dict  # << This is a little dangerous but the get_structure code above looks quite clean
-
-        # Gather resolution information, etc, from the 
-        structure_info_dict = mine_structure_info_from_mmCIF(mmcif_dict)
-    else:
-        pdb_parser = PDBParser()
-        structure = pdb_parser.get_structure(structure_id,structure_fin)
-        # If we ever return to deposited PDBs for ddg calculations (we now use mmcif), then this has to
-        # be worked out.  For now, the only .pdb files we are processing are models that will not have
-        # spurious residues that need to be cleaned out
-        mmcif_dict = None
+else:
+    try:
+        structure_id,structure,structure_info_dict,mmcif_dict = ddg_load_structure(args,config_dict)
+    except LoadStructureError as exception:
+        ddg_repo.psb_status_manager.sys_exit_failure(exception.message)
 
 
     ddg_cleaner = ddg_clean.DDG_Cleaner(structure,mmcif_dict,True,species_filter=args.species_filter)
     cleaned_structure, residue_to_clean_xref = ddg_cleaner.clean_structure_for_ddg(False, [args.chain])
-        
+
+
     from Bio.PDB import PDBIO
     pdbio = PDBIO()
     cleaned_structure_temp_filename = None
