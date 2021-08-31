498e1039dec19741348540da537c3b237e495398
Chris Moth <chris.moth@vanderbilt.edu>
2021-08-31 17:07:18 -0500

    Add symbolic link to perl scripts to container build time #28

--- build_docker_images.bash
+++ build_docker_images.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Script to create "final" pipeline docker image file chrismoth/image_phase9 
+# Must be run as root
+# Requires that Rosetaa 3.13 and 3.7 files have been copied in to the bulid area
+# Using hte xfer_rosetta*.bash sciprts
+# Also requires that chimera_headless has been copied in
 rm -f image_phase9.simg
-docker build --rm -f Dockerfile.phase0 -t chrismoth/image_phase0 . 2>&1 | tee image_phase0.stderr_out  && \
-docker build --rm -f Dockerfile.phase1 -t chrismoth/image_phase1 . 2>&1 | tee image_phase1.stderr_out  && \
-docker build --rm -f Dockerfile.phase2 -t chrismoth/image_phase2 . 2>&1 | tee image_phase2.stderr_out  && \
-docker build --rm -f Dockerfile.phase3 -t chrismoth/image_phase3 . 2>&1 | tee image_phase3.stderr_out  && \
-docker build --rm -f Dockerfile.phase9 -t chrismoth/image_phase9 . 2>&1 | tee image_phase9.stderr_out 
+docker build --rm -f docker/Dockerfile.0.centos_anaconda -t chrismoth/image_phase0 . 2>&1 | tee image_phase0.stderr_out  && \
+docker build --rm -f docker/Dockerfile.1.perl            -t chrismoth/image_phase1 . 2>&1 | tee image_phase1.stderr_out  && \
+docker build --rm -f docker/Dockerfile.2.ensembl_bioperl -t chrismoth/image_phase2 . 2>&1 | tee image_phase2.stderr_out  && \
+docker build --rm -f docker/Dockerfile.3.chimera         -t chrismoth/image_phase3 . 2>&1 | tee image_phase3.stderr_out  && \
+docker build --rm -f docker/Dockerfile.9.pip_rosetta     -t chrismoth/image_phase9 . 2>&1 | tee image_phase9.stderr_out 
