3ab4e77bff97cdf1874c37ee53b6a2b030b18766
Chris Moth <chris.moth@vanderbilt.edu>
2021-09-19 14:25:11 -0500

    tweak personal development singularity run script mounts #28

--- run_singularity.bash
+++ run_singularity.bash
@@ -1,1 +1,1 @@
-singularity exec --env ASDF=fdsa --bind /home/mothcw/UDNlocal/:/UDN/,/dors/capra_lab/data:/data/,/TB4/mothcw/vep_cache:/vep_cache/ image_phase9.simg /bin/bash --norc --noprofile
+singularity exec --env ASDF=fdsa --bind /home/mothcw/UDNlocal/:/UDN/,/dors/capra_lab/data:/data/,/TB4/mothcw/vep_cache:/vep_cache/,/TB4:/TB4 image_phase9.simg /bin/bash --norc --noprofile
