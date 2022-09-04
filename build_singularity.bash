03fa31641626ce231152aa76cb07266b18eaa07f
Chris Moth, Ph.D <30936704+ChrisMoth@users.noreply.github.com>
2022-09-04 16:16:10 -0500

    Update build_singularity.bash
    Remind user to push docker image before building singularity simg file

--- build_singularity.bash
+++ build_singularity.bash
@@ -1,4 +1,7 @@
 #!/bin/bash 
+echo ======================================================
+echo Reminder: Did you pugh image_phase9 up to he cloud?
+echo ======================================================
 rm -f image_phase9.simg
 singularity build --docker-login image_phase9.simg Singularity
 # had pull before
