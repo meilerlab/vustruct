ab6e6d318d2f681e6ecf1c569e52643c0845c28f
Chris Moth <Chris.Moth@vanderbilt.edu>
2023-01-06 12:38:16 -0600

    Correct tabs to PEP8 #48

--- bin/psb_shared/psb_perms.py
+++ bin/psb_shared/psb_perms.py
@@ -3,40 +3,34 @@ import os
 import grp
 import stat
 
+
 class PsbPermissions(object):
 
-  def __init__(self,config_dict):
-    self.chgrp_to = config_dict['chgrp_to']
-    self.gr_gid = grp.getgrnam(self.chgrp_to).gr_gid
+    def __init__(self, config_dict):
+        self.chgrp_to = config_dict['chgrp_to']
+        self.gr_gid = grp.getgrnam(self.chgrp_to).gr_gid
 
+    def set_dir_group_and_sticky_bit(self, dirname):
+        """Attempt to mark a directory as being owned by the configured group.
+        Then, attempt to mark the directory's sticky bit so that this group
+        id propagates to any files created inside this directory"""
+        return
 
+        try:
+            os.chown(dirname, -1, self.gr_gid)
+        except OSError:
+            pass
 
-  def set_dir_group_and_sticky_bit(self,dirname):
-    """Attempt to mark a directory as being owned by the configured group.
-    Then, attempt to mark the directory's sticky bit so that this group
-    id propagates to any files created inside this directory"""
-    return
+        # Setting the sticky bit on directories also fantastic
+        # Simultaneously, make sure users and group have full
+        # Read/Write/Execute on the directory
+        try:
+            os.chmod(dirname,
+                     stat.S_IRWXU | stat.S_IRWXG | stat.S_ISGID)
+        except OSError:
+            pass
 
-    try:
-      os.chown(dirname, -1, self.gr_gid)
-    except OSError:
-      pass
+    def makedirs(self, dest_path):
+        os.makedirs(dest_path, exist_ok=True)
 
-    # Setting the sticky bit on directories also fantastic
-    # Simultaneously, make sure users and group have full
-    # Read/Write/Execute on the directory
-    try:
-      os.chmod(dirname,
-               stat.S_IRWXU | stat.S_IRWXG | stat.S_ISGID)
-    except OSError:
-      pass
-
-  def makedirs(self,dest_path):
-    if not os.path.exists(dest_path):
-      os.makedirs(dest_path)
-    try:
-      assert os.path.exists(dest_path)
-    except:
-      raise OSError("PsbPermissions:Failure to create missing directory path", dest_path)
-    self.set_dir_group_and_sticky_bit(dest_path)
-
+        self.set_dir_group_and_sticky_bit(dest_path)
