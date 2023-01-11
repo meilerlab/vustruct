08754d1ddf334845bb5397ce295e226d15b7e396
Chris Moth <Chris.Moth@vanderbilt.edu>
2023-01-11 12:09:27 -0600

    Remove unused file

--- webfront/run_vustruct_flask.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-export FLASK_APP=./vustruct_flask
-export UDN=/dors/capra_lab/users/mothcw/UDNtests
-export PYTHONPATH=~/psbadmin/pdbmap:~/psbadmin/bin
-
-# export FLASK_ENV=development
-flask run
