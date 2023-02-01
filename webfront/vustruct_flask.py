b33ffa112fd5b23553330944ec4b1c978e0bc33f
Chris Moth <Chris.Moth@vanderbilt.edu>
2023-02-01 16:39:44 -0600

    Revamp the flask launcher to embrace all phases of the pipeline #60

--- webfront/vustruct_flask.py
+++ webfront/vustruct_flask.py
@@ -4,25 +4,36 @@
 #
 # Via flask, implements RESTAPI calls which
 # 
-# 1) return unique job identifiers and
+# 1) return unique job identifiers get_uuid()
 # 2) launch the needed psb_*.py command line programs on the compute cluster
 # 3) record and report emerging website availability
 # 4) inform vustruct_webupdate of running job IDs which are needing a web refresh.
 # 
 #     Because vustruct_flask will launch prep tasks and cluster tasks, 
 #     vustrucT_flask is executed outside the container.
+#
+# curl samples can be found with the various calls, just search for "curl"
+# in the source code below
 
 import logging
+
+import flask
 from flask import Flask
+# from flask import Request
 from flask import request
 from flask import jsonify
 import uuid
 import threading
+from datetime import datetime
 import time
 import json
 import subprocess
 import os
+import sys
+
+# requests is used to read the excel or missense filess uploaded to wordpress.
 import requests
+from requests_file import FileAdapter
 from typing import Dict
 
 # import time
@@ -45,7 +56,7 @@ from psb_shared import psb_config
 from psb_shared import psb_perms
 
 # UDN="/dors/capra_lab/users/mothcw/UDNtests/"
-UDN=os.getenv("UDN")
+UDN = os.getenv("UDN")
 if not UDN:
     errormsg = "UDN environment variable must be defined before launch of %s" % UDN
     sys.exit(errormsg)
@@ -54,12 +65,6 @@ if not UDN:
 cmdline_parser = psb_config.create_default_argument_parser(__doc__, os.path.dirname(os.path.dirname(__file__)))
 
 args, remaining_argv = cmdline_parser.parse_known_args()
-
-if args.debug:
-    sh.setLevel(logging.DEBUG)
-elif args.verbose:
-    sh.setLevel(logging.INFO)
-
 
 sh = logging.StreamHandler()
 LOGGER = logging.getLogger()
@@ -70,44 +75,75 @@ LOG_FORMAT_STRING = '%(asctime)s %(levelname)-4s [%(filename)16s:%(lineno)d] %(m
 DATE_FORMAT_STRING = '%H:%M:%S'
 log_formatter = logging.Formatter(LOG_FORMAT_STRING, DATE_FORMAT_STRING)
 
-LOGGER.setLevel(logging.DEBUG)
-sh.setLevel(logging.WARNING)
+# print("Level is %s " % os.getenv("FLASK_ENV"))
+if args.debug or (os.getenv("FLASK_ENV") == 'development'):
+    print("Setting Debug level")
+    sh.setLevel(logging.DEBUG)
+elif args.verbose:
+    sh.setLevel(logging.INFO)
+else:
+    sh.setLevel(logging.WARNING)
 sh.setFormatter(log_formatter)
 
-LOGGER.info("%s running web-input cases in %s" , __file__, UDN)
+LOGGER.info("%s running web-input cases in %s", __file__, UDN)
 LOGGER.info("Initializing Flask(%s)", __name__)
 app = Flask(__name__)
 
 jobs_dict = {}
 jobs_needing_website_refresh = []
 
+
 class ActiveVUstructJob:
     def __init__(self, case_id: str, job_uuid: str) -> None:
         self.case_id = case_id
         self.job_uuid = job_uuid
-        self.working_directory = os.path.join(UDN,"external_user_%s_%s" % (case_id, job_uuid))
-  
-    def launch_parse_udn_report(self, excel_file_url: str):
-        os.makedirs(self.working_directory,exist_ok=True)
+        # As we launch processes, we populate this with
+        # the vustruct "proprocess", "plan", "launch", "report"
+        self.last_module_launched = ""
+        self.last_module_returncode = 0
+        self.excel_file_URI = ""
+
+        # The running of psb_rep is particularly important.
+        self.last_psb_rep_start = ""
+        self.last_psb_rep_end = ""
+
+        self.working_directory = os.path.join(UDN, "external_user_%s_%s" % (case_id, job_uuid))
+        self.data_format = ""
+
+        self.init_time = datetime.now().isoformat()
+
+    def launch_parse_udn_report(self) -> int:
+        os.makedirs(self.working_directory, exist_ok=True)
         save_cwd = os.getcwd()
-        print("chdir(%s)" % self.working_directory)
+        LOGGER.info("chdir(%s)", self.working_directory)
         os.chdir(self.working_directory)
-        print("Attempting to load uploaded spreadsheet from %s" % excel_file_url)
-        response = requests.get(excel_file_url);
+        LOGGER.info("Attempting to load uploaded spreadsheet from %s", self.excel_file_URI)
+
+        # Usually we "get" the spreadsheet from wordpress using http
+        # It is convenient to be able to test all this with local files
+        _session = requests.Session()
+        _session.mount('file://', FileAdapter())
+        response = _session.get(self.excel_file_URI)
+
+        # Write out the UDN spreadsheet in the final directory where it will be run
         with open("external_user_%s_%s.xlsx" % (self.case_id, self.job_uuid), "wb") as spreadsheet_f:
             spreadsheet_f.write(response.content)
 
         launch_parse_command = "parse_udn_report.py"
-        print("Running: %s" % launch_parse_command)
+        LOGGER.info("Running: %s" % launch_parse_command)
+
         launch_parse_return = \
             subprocess.run(launch_parse_command, shell=False,
                        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        print("%s finished" % launch_parse_command)
+        LOGGER.debug("%s finshed with exit code", launch_parse_command, launch_parse_return.returncode)
         os.chdir(save_cwd)
 
-        return launch_parse_return
+        self.last_module_launched = "parse"
+        self.last_module_returncode = launch_parse_return.returncode
 
-    def launch_psb_rep(self):
+        return self.last_module_returncode
+
+    def launch_psb_rep(self) -> int:
         save_cwd = os.getcwd()
         print("chdir(%s)" % self.working_directory)
         os.chdir(self.working_directory)
@@ -115,38 +151,58 @@ class ActiveVUstructJob:
 
         launch_psb_rep_command = "psb_rep.py"
         print("Running: %s" % launch_psb_rep_command)
+
+        self.last_psb_rep_start = datetime.now().isoformat()
         launch_psb_rep_return = \
             subprocess.run(launch_psb_rep_command, shell=False,
                        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         print("%s finished" % launch_psb_rep_command)
+        self.last_psb_rep_end = datetime.now().isoformat()
         os.chdir(save_cwd)
+        jobs_needing_website_refresh.append(self.job_uuid)
 
-        return launch_psb_rep_return
-
-
-
+        return launch_psb_rep_return.returncode
 
 
-def launch_vustruct_case(response_json: Dict[str,str]) -> subprocess.CompletedProcess :
+def launch_vustruct_case_thread(vustruct_job: ActiveVUstructJob) -> subprocess.CompletedProcess:
+    """
+    This function is launched via the thread API.  "int he background" it will run the various components
+    of the entire pipeline, and return in case of terminal failure, or completion.
+    Along the way, the website files will slated for updated
+    """
     global jobs_needing_website_refresh
 
-    active_vustruct_job = ActiveVUstructJob(
-        response_json['case_id'],
-        response_json['job_uuid']
-    )
-   
-    launch_parse_retcd = 0 
-    if 'data_format' in response_json and response_json['data_format'] == 'Vanderbilt UDN Case Spreadsheet':
-        launch_parse_retcd = active_vustruct_job.launch_parse_udn_report(response_json['excel_file_URL'])
-        if launch_parse_retcd != 0:
-            print("UUGH - parse_udn_Report for %s failed with %s" % (response_json['job_uuid'], launch_parse_retcd))
+    jobs_dict[vustruct_job.job_uuid] = vustruct_job
 
-    launch_psb_rep_retcd = active_vustruct_job.launch_psb_rep()
+    # If we need to preprocess (parse) an input to create a missense file
+    # Jump on that first
+    vustruct_job.last_module_returncode = 0
+    if vustruct_job.data_format == 'Vanderbilt UDN Case Spreadsheet':
+        vustruct_job.last_module_launched = 'preprocess'
+        vustruct_job.last_module_returncode = vustruct_job.launch_parse_udn_report()
+        vustruct_job.last_module_error_message = ""
+        if vustruct_job.last_module_returncode != 0:
+            vustruct_job.last_module_error_message = "LATER Add error message for parse_udn_report please"
+        # After we complete the parse step, we definitely need to run the psb_rep program
+        # so that the OTHER flask program will install the growing website if it comes to that.
+        # HOWEVER, the psb_rep program must note if the parse process has terminated with bad exit code
+        # and take that into consideration.  IT may not as of yet.
+        launch_psb_rep_retcd = vustruct_job.launch_psb_rep()
+
+        if launch_psb_rep_retcd != 0:
+            vustruct_job.last_report_error = "psb_rep.py for %s failed with %s" % \
+                                             (vustruct_job.job_uuid, launch_psb_rep_retcd)
+
+
+        if vustruct_job.last_module_returncode != 0:
+            return
+
+    launch_psb_rep_retcd = vustruct_job.launch_psb_rep()
   
     if launch_psb_rep_retcd == 0:
-        jobs_needing_website_refresh.append(response_json['job_uuid'])
+        jobs_needing_website_refresh.append(vustruct_job.job_uuid)
     else:
-        print("UUGH - psb_rep for %s failed with %s" % (response_json['job_uuid'], launch_psb_rep_retcd))
+        print("UUGH - psb_rep for %s failed with %s" % (vustruct_job.job_uuid, launch_psb_rep_retcd))
 
     return launch_psb_rep_retcd
 
@@ -161,26 +217,70 @@ def launch_vustruct_case(response_json: Dict[str,str]) -> subprocess.CompletedPr
 #         subprocess.run(psb_plan_command, shell=True,
 #                        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
  
-@app.route('/get_uuid', methods = ['POST', 'GET'])
+
+@app.route('/get_uuid', methods=['POST'])
 def get_uuid():
-   print ("get_uuid(): The request I got is: %s" % (request.json))
-   # Launch psb_plan.py
+    """
+    This first REST API call from the Wordpress functions.php
+    SIMPLY verifies that this FLASK module is listening.
 
-   uuid_str = str(uuid.uuid4())
+    We do not record the uuid here in FLASK.  We return a new UUID
+    and the Wordpress code will _later_ call launch_vustruct() when
+    all checks out on the UI side and SUBMIT is pressed there.
 
-   return jsonify({"job_uuid": uuid_str}) # 'Hello ' #  + rq.get('name', 'No name')
+    You can test this RESTAPI call easily with:
+    curl -X POST http://localhost:5000/get_uuid
+    """
+
+    # LOGGER.info ("get_uuid(): The request I got is: %s" % (request.json))
+    # Launch psb_plan.py
+
+    uuid_str = str(uuid.uuid4())
+
+    return jsonify({"job_uuid": uuid_str})  # 'Hello ' #  + rq.get('name', 'No name'))
 
 @app.route('/launch_vustruct', methods = ['POST', 'GET'])
 def launch_vustruct():
-   print ("launch_vustruct(): The request I got is: %s" % (request.json))
-   psb_plan_thread = threading.Thread(target=launch_vustruct_case, args=(request.json,))
-   psb_plan_thread.start()
+    """
+    There are a variety of ways to launch a new VUstruct case - 
+    Some start frmo the missense.csv.  Others from a spreadsheet format.
+    
+    An example curl command would be:
+    curl -X POST http://localhost:5000/launch_vustruct \ 
+        -H 'Content-Type: application/json' \
+        -d'{"data_format": "Vanderbilt UDN Case Spreadsheet",
+            "job_uuid": "12435",
+            "case_id": "test",
+            "excel_file_URI": "file:////dors/capra_lab/users/mothcw/UDNtests/fakecase0/fakecase0.xlsx"}'
+    """
 
-   # Send back the entire json that was send to us AnD add a launch_status to the gemisch
-   json_to_return_after_launch = request.json | {"launch_status": "VUstruct now running on accre cluster"}
-   print("Sending back this json %s" % json_to_return_after_launch)
 
-   return jsonify(json_to_return_after_launch)
+    LOGGER.debug ("/launch_vustruct: request.json=%s", str(request.json))
+
+    for required_key in ['case_id', 'job_uuid', 'data_format']:
+        if not required_key in request.json:
+            return "Request JSON is missing required key: " + required_key
+
+    vustruct_job = ActiveVUstructJob(
+        case_id=request.json['case_id'],
+        job_uuid=request.json['job_uuid'])
+    vustruct_job.data_format = request.json['data_format']
+
+    if 'excel_file_URI' in request.json:
+        vustruct_job.excel_file_URI = request.json['excel_file_URI']
+
+    # This so far has been very quick.  Now run the background thread to run the entire pipeline
+    # Return to caller immediately of course.
+
+    psb_plan_thread = threading.Thread(target=launch_vustruct_case_thread, args=(vustruct_job,))
+    psb_plan_thread.start()
+
+    # Send back the entire json that was send to us AnD add a launch_status to the gemisch
+    # json_to_return_after_launch = request.json | {"launch_status": "VUstruct now running on accre cluster"}
+    vustruct_job_json = jsonify(vars(vustruct_job))
+    LOGGER.debug("Sending back this json %s", vustruct_job_json )
+
+    return vustruct_job_json
 
 @app.route('/jobs_needing_refresh', methods = ['POST', 'GET'])
 def jobs_needing_refresh():
@@ -189,3 +289,17 @@ def jobs_needing_refresh():
     # Reset the listof jobs needing website refresh so we don't keep going and going
     jobs_needing_website_refresh = []
     return json_jobs_needing_website_refresh
+
+@app.route('/get_all_jobs', methods = ['POST'])
+def get_all_jobs():
+    return jsonify([vars(active_vustruct) for active_vustruct in jobs_dict.values()])
+
+@app.route('/add_job', methods = ['POST'])
+def add_job():
+    new_job_info = request.json
+    LOGGER.info("add_job request json = %s" % new_job_info)
+    active_vustruct = ActiveVUstructJob(new_job_info['case_id'], new_job_info['job_uuid'])
+    jobs_dict[new_job_info['job_uuid']] = active_vustruct
+
+
+    return jsonify(vars(active_vustruct))
