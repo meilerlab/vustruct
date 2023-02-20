f610c12cae8a9c75efe46e45051edf93d8e508e2
Chris Moth <chris.moth@vanderbilt.edu>
2023-02-20 15:27:36 -0600

    Implement Authorization Token per Eric.  Still need to remove token from source #60

--- webfront/run_vustruct_flask.bash
+++ webfront/run_vustruct_flask.bash
@@ -2,9 +2,11 @@
 # A sample of how to run vustruct_flask from bash shell
 export FLASK_APP=./vustruct_flask
 export UDN=/dors/capra_lab/users/mothcw/UDNtests
-export PYTHONPATH=~/psbadmin/pdbmap:~/psbadmin/bin
+export PATH=/dors/capra_lab/users/mothcw/psbadmin/bin:$PATH
+export PYTHONPATH=/dors/capra_lab/users/mothcw/psbadmin/pdbmap:/dors/capra_lab/users/mothcw/psbadmin/bin
 
 export FLASK_ENV=development
-cmd='flask run --no-reload -p 5000'
+cmd='flask run --no-reload -p 3080 --host="0.0.0.0"'
+# cmd='flask run --no-reload -p 3080 --host="127.0.0.1"'
 echo "Launching: $cmd"
 eval $cmd
