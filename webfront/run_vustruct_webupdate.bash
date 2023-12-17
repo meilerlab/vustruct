36148c8e007002f7624192ea4a387a91b6431cb5
Chris Moth <chris.moth@vanderbilt.edu>
2023-12-17 15:38:18 -0600

    Move web cases from UDNtests to VUStruct directory #60

--- webfront/run_vustruct_webupdate.bash
+++ webfront/run_vustruct_webupdate.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # A sample of how to run vustruct_flask from bash shell
 export FLASK_APP=./vustruct_webupdate
-export UDN=/dors/capra_lab/users/mothcw/UDNtests
+export UDN=/dors/capra_lab/users/mothcw/VUStruct
 export PYTHONPATH=~/psbadmin/pdbmap:~/psbadmin/bin
 
 export FLASK_ENV=development
