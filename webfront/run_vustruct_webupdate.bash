451ef2876d3a5c457667241e01be9b2925b6e498
Chris Moth <chris.moth@vanderbilt.edu>
2024-07-10 16:07:49 -0500

    Update flask debug config, loop on running the two vustruct_flask.. apps #60

--- webfront/run_vustruct_webupdate.bash
+++ webfront/run_vustruct_webupdate.bash
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 # A sample of how to run vustruct_flask from bash shell
+set -x
 export FLASK_APP=./vustruct_webupdate
 export UDN=/dors/capra_lab/users/mothcw/VUStruct
-export PYTHONPATH=~/psbadmin/pdbmap:~/psbadmin/bin
+export PYTHONPATH=/dors/capra_lab/users/mothcw/psbadmin/pdbmap:/dors/capra_lab/users/mothcw/psbadmin/bin
 
+# export FLASK_ENV=development
 export FLASK_ENV=development
-cmd='flask run --no-reload -p 3000'
+export FLASK_DEBUG=True
+cmd='flask --debug run --no-reload -p 3000'
 echo "Launching: $cmd"
 eval $cmd
