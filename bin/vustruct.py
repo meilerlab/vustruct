6b97b7b2b23cc8f8e277473900f2feee22672c58
Chris Moth <chris.moth@vanderbilt.edu>
2024-07-10 15:47:12 -0500

    For prepocess and psb_plan.py steps, now capture the critical and error log entries as user error in case of problems #60

--- bin/vustruct.py
+++ bin/vustruct.py
@@ -8,11 +8,13 @@
 # Email          : chris.moth@vanderbilt.edu
 # Date           : 2023-01
 # Description
-# class VUstruct, in essence, oversees all the individual VUstruct
-# pipeline command line components.
 #
-# Each psb_*.py command line program "reports in" to class VUstruct
-# as progress is made.  VUstruct stores records of the invocation
+# class VUStruct, in essence, oversees all the individual VUStruct
+# pipeline command line components:
+# 'preprocess', 'plan', 'launch', 'report'
+#
+# Each psb_*.py command line program "reports in" to class VUStruct
+# as progress is made.  VUStruct stores records of the invocation
 # of each vustruct main program, the location of generated  log files,
 # and other details helpful to a final website, which must include
 # user-accessible log files to replace tradition .log files accessible
@@ -20,7 +22,7 @@
 #
 # These storage records are written to the file $case_id + "_vustruct" + ".json"
 #
-# The individual pipeline programs tracked by class VUstruct are:
+# The individual pipeline programs tracked by class VUStruct are:
 #
 # 0) Optional Genomic Coordinates preprocessing:
 #         parse_udn_report.py, parse_wustl.py, or vcf2missense.py
@@ -28,11 +30,21 @@
 # 2) psb_launch.py
 # 3) psb_monitor.py
 # 3) psb_report.py
-
+#
+# In addition to various member functions which can answer the status of the case
+# and early progress results...
+# The final 'output' of this class is a dictionary that can be fed to
+# a jinja2 template as part of website creation, to display the various
+# logs and so forth from the run of all the modules.
+#
+# class VUStruct is independent of class VUS_flask_case_manager (vustruct_flask.py)
+# VUStruct instances evolve on disk in the same way, whether applications are 
+# run at the command line or via the website (vustruct_flask.py)
 # =============================================================================#
 
 import logging
 from logging.handlers import RotatingFileHandler
+# from logging.handlers import FileHandler
 
 import json
 import copy
@@ -40,16 +52,16 @@ import sys
 import os
 from datetime import datetime
 
-
-class VUstruct:
+class VUStruct:
 
     def __init__(self, vustruct_module: str, case_id: str, executable_file: str):
         """Create logging streams compatible with web-site building in the log/
            directory.  Also setup an exception handler that routes unhandled
            exceptions to the logs, on the way to program exit"""
 
-        self._stream_handler = None
-        self._rotating_file_handler = None
+        self._stream_handler = None             # Outputs to stderr I think
+        self._rotating_file_handler = None      # Detailed logs
+        self._critical_error_handler = None     # File in errorlogs/ useful to display to users on abrupt end of psb_plan.py
 
         self._psb_plan_temp_filehandler = None
 
@@ -73,6 +85,9 @@ class VUstruct:
                 'start_time': '',
                 'end_time': '',
                 'log_filename': '',
+                'critical_error_log_filename': '',
+                'input_filename': '',
+                'outputs': {},
                 'exit_code': 1  # Default file is marked as not exiting well
 
             } for command_line_module in possible_vustruct_modules
@@ -83,8 +98,18 @@ class VUstruct:
         self.read_file()
 
         self.log_filename = ''
+        self.critical_error_log_filename = ''
         self._vustruct_dict[self._module]['executable'] = executable_file
-        sys.excepthook = VUstruct.handle_unhandled_exception_and_exit
+        sys.excepthook = VUStruct.handle_unhandled_exception_and_exit
+
+        old_log_record_factory = logging.getLogRecordFactory()
+        def vustruct_log_record_factory(*args, **kwargs):
+            record = old_log_record_factory(*args,**kwargs)
+            record.vustruct_case_id = case_id
+            return record
+
+        logging.setLogRecordFactory(vustruct_log_record_factory)
+
 
     @staticmethod
     def handle_unhandled_exception_and_exit(exc_type, exc_value, exc_traceback):
@@ -97,6 +122,14 @@ class VUstruct:
             '%(asctime)s %(levelname)-8s [%(filename)20s:%(lineno)-4d] %(message)s',
             datefmt="%H:%M:%S")
 
+    @property
+    def critical_error_log_formatter(self):
+        return logging.Formatter(
+            '-'*80 + '\n' + 
+            'VUStruct Case %(vustruct_case_id)s\n%(levelname)s error while processing input: %(asctime)s in program %(filename)s at line %(lineno)d\n%(message)s' +
+            '\n' + '-'*80,
+            datefmt="%H:%M:%S")
+
 
     def initialize_file_and_stderr_logging(self, debug: bool) -> str:
         """
@@ -105,20 +138,23 @@ class VUstruct:
         """
         assert self._rotating_file_handler == None
         assert self._stream_handler == None
+        assert self._critical_error_handler == None
+
         program_name = os.path.splitext(os.path.basename(self.executable_file))[0]
         self.log_filename = os.path.join("./log", "%s.log" % program_name)
+        self.critical_error_log_filename = os.path.join("./log", "%s.critical_error.log" % program_name)
+
+        # If the main log needs a file roll, note that and we'll get to it...
         need_roll = os.path.isfile(self.log_filename)
 
         # Build out any directories down to the ./log directory
         os.makedirs(os.path.dirname(self.log_filename), exist_ok=True)
-        sys.stderr.write("VUstruct log file set to: %s\n" % self.log_filename)
+        sys.stderr.write("VUStruct log file set to: %s\n" % self.log_filename)
 
         root_logger = logging.getLogger()
-
-
-
-
         root_logger.setLevel(logging.DEBUG)
+
+        # We typically log INFO and above to console
         self._stream_handler = logging.StreamHandler()
         self._stream_handler.setLevel(logging.INFO)
         log_formatter = logging.Formatter('%(asctime)s %(levelname)-8s [%(filename)20s:%(lineno)-4d] %(message)s',
@@ -126,6 +162,7 @@ class VUstruct:
         self._stream_handler.setFormatter(self.standard_log_formatter)
         root_logger.addHandler(self._stream_handler)
 
+        # In the saved file, we capture DEBUG level and above
         self._rotating_file_handler = RotatingFileHandler(self.log_filename, backupCount=7)
         self._rotating_file_handler.setLevel(logging.DEBUG)
         root_logger.addHandler(self._rotating_file_handler)
@@ -150,6 +187,13 @@ class VUstruct:
         except:
             root_logger.info(f"Unable to complete os.symlink('{self.log_filename}', '{log_symlink}')")
             pass
+
+        # We need one more log file, this one ONLY for the critical and error entries
+        # We create this file anew with every run - not an append
+        self._critical_error_handler = logging.FileHandler(self.critical_error_log_filename, mode='w')
+        self._critical_error_handler.setLevel(logging.ERROR)
+        root_logger.addHandler(self._critical_error_handler)
+        self._critical_error_handler.setFormatter(self.critical_error_log_formatter)
 
 
         return self.log_filename
@@ -185,11 +229,12 @@ class VUstruct:
         logging.getLogger().removeHandler(self._psb_plan_temp_filehandler)
         self._psb_plan_temp_filehandler = None
 
-    # The final 'output' of this class is a dictionary that can be fed to
-    # a jinja2 template as part of website creation, to display the various
-    # logs and so forth from the run of all the modules.
     def dict_for_jinja2(self):
-        return copy.deepcopy(self._vustruct_dict)
+        # Return all information about all planning and launch phases
+        # for report generation
+        jinja2_dict = copy.deepcopy(self._vustruct_dict)
+        return jinja2_dict
+        
 
     def stamp_start_time(self):
         self._vustruct_dict[self._module]['start_time'] = datetime.now().isoformat()
@@ -223,8 +268,67 @@ class VUstruct:
         self._vustruct_dict[self._module]['log_filename'] = log_filename
 
     @property
-    def preprocess(self):
+    def input_filename(self):
+        return self._vustruct_dict[self._module]['input_filename']
+
+    @input_filename.setter
+    def input_filename(self, input_filename):
+        self._vustruct_dict[self._module]['input_filename'] = input_filename
+
+    @staticmethod
+    def _read_failure_info_file(self_preprocess_or_plan) -> list[str]:
+        critical_error_log_filename = os.path.join("./log", 
+            "%s.critical_error.log" % os.path.basename(self_preprocess_or_plan['executable']).split('.')[0])
+
+        failure_info = []
+        try:
+            with open(critical_error_log_filename) as f:
+                for line in f:
+                    failure_info.append(line.rstrip())
+        except Exception as ex:
+            failure_info =  ["Unable to open %s:" % critical_error_log_filename] + str(ex).split('\n')
+
+        return failure_info
+
+    @property
+    def preprocess(self) -> dict:
         return self._vustruct_dict['preprocess']
+
+    @property
+    def preprocess_attempted(self) -> bool:
+        return bool(self.preprocess['executable']) 
+
+    @property 
+    def preprocess_failed(self) -> bool:
+        # If the preorocessor was invoked, but it failed
+        # then we need to return that message to the report generator
+        # caller who can 
+        return (self.preprocess_attempted and 
+               self.preprocess['exit_code'] and 
+               self.preprocess['exit_code'] != '0')
+
+    @property 
+    def preprocess_failure_info(self) -> list[str]:
+        return self._read_failure_info_file(self.preprocess)
+
+    @property
+    def plan_attempted(self) -> bool:
+        return bool(self.plan['executable']) 
+
+    @property 
+    def plan_failed(self) -> bool:
+        # If the preorocessor was invoked, but it failed
+        # then we need to return that message to the report generator
+        # caller who can 
+        return (self.plan_attempted and 
+               self.plan['exit_code'] and 
+               self.plan['exit_code'] != '0')
+
+
+    @property
+    def plan_failure_info(self) -> list[str]:
+        return self._read_failure_info_file(self.plan)
+
     @property
     def plan(self):
         return self._vustruct_dict['plan']
