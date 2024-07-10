db24ac8b8d443199f22b6191c0a96b1910ac3754
Chris Moth <chris.moth@vanderbilt.edu>
2024-07-10 15:59:34 -0500

    continue to improve permissions setting in the cluster filesystem #82

--- bin/psb_shared/psb_perms.py
+++ bin/psb_shared/psb_perms.py
@@ -2,6 +2,11 @@
 import os
 import grp
 import stat
+from pathlib import Path
+
+import logging
+LOGGER = logging.getLogger(__name__)
+
 
 
 class PsbPermissions(object):
@@ -9,6 +14,39 @@ class PsbPermissions(object):
     def __init__(self, config_dict):
         self.chgrp_to = config_dict['chgrp_to']
         self.gr_gid = grp.getgrnam(self.chgrp_to).gr_gid
+
+    def os_open(self, filename: str, read_or_write: str) -> int:
+        """
+        Return an integer filedescriptor to a file opened with the permissions
+        and group ownership known to ddg_repo through ddg_repo's initialization
+        """
+        assert read_or_write == 'r' or read_or_write == 'w'
+
+        # file_create_mode =(self._file_create_mode if (read_or_write == 'w') else 0)
+        file_create_mode = 0o660
+
+        # old_umask = os.umask(0)
+
+        fd = os.open(filename,
+                     (os.O_CREAT | os.O_WRONLY | os.O_TRUNC) if (read_or_write == 'w') else os.O_RDONLY,
+                       )
+
+        if fd < 0:
+            message = "Unable to os.open(%s,%s)"%(filename,read_or_write)
+            LOGGER.critical(message)
+            sys.exit(message)
+        LOGGER.info("Success: os.open(%s,%s,'%s') returning %d",
+                    os.path.abspath(filename),
+                    oct(file_create_mode),
+                    read_or_write,
+                    fd)
+        # self.set_group(filename)
+        # os.umask(old_umask)
+        return fd
+
+    def touch(self,filename: str):
+        Path(filename).touch(exist_ok=True,mode=0o770)
+
 
     def set_dir_group_and_sticky_bit(self, dirname):
         """Attempt to mark a directory as being owned by the configured group.
@@ -31,6 +69,6 @@ class PsbPermissions(object):
             pass
 
     def makedirs(self, dest_path):
-        os.makedirs(dest_path, exist_ok=True)
+        os.makedirs(dest_path, mode=0o777, exist_ok=True)
 
         self.set_dir_group_and_sticky_bit(dest_path)
