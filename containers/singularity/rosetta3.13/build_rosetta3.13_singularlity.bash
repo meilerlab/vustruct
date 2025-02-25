d711f088bb803c600367f6e8ab889d12146bb9c2
Chris Moth, Ph.D. <30936704+ChrisMoth@users.noreply.github.com>
2025-02-25 16:07:50 -0600

    Update build_rosetta3.13_singularlity.bash

--- containers/singularity/rosetta3.13/build_rosetta3.13_singularlity.bash
+++ containers/singularity/rosetta3.13/build_rosetta3.13_singularlity.bash
@@ -1,1 +1,6 @@
-
+#!/usr/bin/bash
+echo Running Singularity build to create rosetta3.13_ddGCartesian.sif container image file
+set -e
+cmd='singularity build --fakeroot --docker-login Rosetta3.13_ddGCartesian.sif Rosetta3.13_ddGCartesian.def'
+echo "Executing: $cmd"
+eval $cmd
